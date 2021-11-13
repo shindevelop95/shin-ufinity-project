c7d5fce59b63f01646174bd0168bee9ce321c5c8
shin <xinlong95@hotmail.com>
2021-11-13 21:59:12 +0800

    added postList

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="post">
+        <h3>{{post.title}}</h3>
+        <p>{{snippet}}</p>
+    </div>
+</template>
+<script>
+import {computed, onMounted, onUnmounted, onUpdated} from 'vue'
+export default {
+    props:['post'],
+    setup(props){
+        onMounted(() => console.log('mounted'));
+        onUnmounted(() => console.log('component unmounted'))
+        onUpdated(() => console.log('component updated'))
+        console.log("show me the propss",props)
+        const snippet = computed(() => {
+            return props.post.body.substring(0,100) + '...'
+        })
+        return {snippet}
+    }
+
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,16 +6,27 @@
     <p>{{obj.name}} -- {{obj.age}}</p>
     <button @click="updateAge"></button>
     <input type="text" v-model="name">
+    <input type="text" v-model="search">
+    <p>Search term - {{search}}</p>
+    <div v-for="p in searchResult" :key="p">
+        {{p}}
+      </div>
   </div>
+  <PostList v-if="showPost" :posts="posts"/>
+  <button @click="showPost = !showPost">Toggle me</button>
+  <button @click="posts.pop()">delete</button>
 </template>
 
 <script>
-import {reactive, ref} from 'vue'
+import PostList from '../components/PostList.vue'
+import {computed,reactive, ref} from 'vue'
 
 export default {
   name: 'Home',
+  components:{PostList},
   //run before any lifecycle hook
   setup(){
+    const search = ref('')
     const name=ref('mario') //reactive value
     const age=ref(30)
     const obj = ref({name:'shin',age:26})
@@ -28,8 +39,19 @@ export default {
      age.value=35
     }
 
+    const posts = ref([
+      {title:'welcome to the blog', body:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled', id:1},
+      {title:'top 5 CSS tips', body:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using', id:2},
+    ])
+
+    const names = ref(['mario','yoshi','luigi','toad','browser','koopa','peach'])
+    const searchResult = computed(() => {
+     return names.value.filter(e => e.includes(search.value))
+    })
+
+    const showPost = ref(true);
     const updateAge = () => obj.value.age++
-    return {updateAge,obj,name, age,handleClick,p}
+    return {names,updateAge,obj,name, age,handleClick,p,search,searchResult,posts,showPost}
   }
 }
 </script>
