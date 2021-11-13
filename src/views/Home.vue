57098cf683ba7d36e22e89b9e84863039887dd1f
shin <xinlong95@hotmail.com>
2021-11-13 20:17:09 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div>my name is {{firstName}}{{lastName}}</div>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  //run before any lifecycle hook
+  setup(){
+    let firstName="shin"
+    let lastName="wang"
+
+    const handleClick = () => console.log('click me')
+    return {firstName,lastName,handleClick}
+  }
+}
+</script>
